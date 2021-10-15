81ffd7cc063f7502813c9ec7f338738c90f4dfa8
Chakrit B <62160148@go.buu.ac.th>
2021-10-15 22:47:15 +0700

    add font

--- application/views/includes/template/header.php
+++ application/views/includes/template/header.php
@@ -17,23 +17,28 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url().'assests\template\soft-ui-dashboard-main'?>/assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="<?php echo base_url().'assests\template\soft-ui-dashboard-main'?>/assets/img/favicon.png">
-  <title>
-    Soft UI Dashboard by Creative Tim
-  </title>
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-  <!-- Nucleo Icons -->
-  <link href="<?php echo base_url().'assests\template\soft-ui-dashboard-main'?>/assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="<?php echo base_url().'assests\template\soft-ui-dashboard-main'?>/assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="<?php echo base_url().'assests\template\soft-ui-dashboard-main'?>/assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- CSS Files -->
-  <link id="pagestyle" href="<?php echo base_url().'assests\template\soft-ui-dashboard-main'?>/assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url() . 'assests\template\soft-ui-dashboard-main' ?>/assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="<?php echo base_url() . 'assests\template\soft-ui-dashboard-main' ?>/assets/img/favicon.png">
+    <title>
+        Soft UI Dashboard by Creative Tim
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap" rel="stylesheet">
+
+    <!-- Nucleo Icons -->
+    <link href="<?php echo base_url() . 'assests\template\soft-ui-dashboard-main' ?>/assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="<?php echo base_url() . 'assests\template\soft-ui-dashboard-main' ?>/assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="<?php echo base_url() . 'assests\template\soft-ui-dashboard-main' ?>/assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="<?php echo base_url() . 'assests\template\soft-ui-dashboard-main' ?>/assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+
 </head>
 
 <body class="">
