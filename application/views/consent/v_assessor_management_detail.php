f44384c67758558002d95ec3dbfb58d1b5768fb3
jxcho <62160165@go.buu.ac.th>
2022-01-25 20:24:56 +0700

    Update v_assessor_management_detail.php
    เพิ่ม Comment และ Confirm button

--- application/views/consent/v_assessor_management_detail.php
+++ application/views/consent/v_assessor_management_detail.php
@@ -1,56 +1,21 @@
 <!--
-=========================================================
-* Soft UI Dashboard - v1.0.3
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    * v_assessor_management_detail
+    * display for assessor management by add or delete information
+    * @input  -
+    * @output -
+    * @author Apinya Phadungkit
+    * Create date 2565-01-25   
+    * Update date 
 -->
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-    <title>
-        Soft UI Dashboard by Creative Tim
-    </title>
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
-    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
 </head>
 
 <body class="g-sidenav-show  bg-gray-100">
-
-
-
-    <!--
-    /*
-    * v_evaluation
-    * display Evaluation list
-    * @input id, emp_id, position
-    * @output -
-    * @author Phatchara Khongthandee and Pontakon Mujit 
-    * @Create date : 2564-08-15
-    */
--->
-
     <!-- CSS -->
     <style>
     #list_table td,
@@ -111,7 +76,7 @@
                             </div>
                             <div class="col-6 text-end">
                                 <a class="btn bg-gradient-info mb-0" href="javascript:;"><i
-                                        class="fas fa-plus"></i>&nbsp;&nbsp;Add New Card</a>
+                                        class="fas fa-plus"></i>&nbsp;&nbsp;Add Assessor</a>
                             </div>
                         </div>
                     </div>
@@ -195,33 +160,18 @@
                                 </tr>
                             </tbody>
                         </table>
-                        <!-- </div> -->
+                        <!-- </div> -->    
                     </div>
+                </div>
+
+                <div class="col-12 text-end">
+                    <a class="btn bg-gradient-success mb-0" href="javascript:;">Confirm</a>
                 </div>
             </div>
         </div>
     
     </div>
     </main>
- 
-    <!--   Core JS Files   -->
-    <script src="../assets/js/core/popper.min.js"></script>
-    <script src="../assets/js/core/bootstrap.min.js"></script>
-    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-    <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-        var options = {
-            damping: '0.5'
-        }
-        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-    </script>
-    <!-- Github buttons -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
 </body>
 
 </html>
