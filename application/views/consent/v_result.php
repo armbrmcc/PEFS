82cf4fec2d472ca5ccf8620df884dffe8dbe0439
62160102 <62160102@go.buu.ac.th>
2022-01-26 19:55:09 +0700

    Create v_result.php

---
+++ application/views/consent/v_result.php
@@ -0,0 +1,12 @@
+<!--
+    v_review
+    display for review list
+    @input -
+    @output -
+    @author Ponprapai
+    Create date 2565-01-25
+    Update date 2565-00-00
+-->
+<h1>
+    Result management (การจัดการผลคะแนน)
+</h1>
