3c5becd1491f2eaa7d2a69872cb1a4a3540d72fe
Chakrit B <62160148@go.buu.ac.th>
2022-04-12 11:38:36 +0700

    update report

--- application/controllers/Report/Report.php
+++ application/controllers/Report/Report.php
@@ -17,6 +17,109 @@ class Report extends MainController
     {
         parent::__construct();
     }
+    /*
+	* show_report
+	* show view report
+	* @input    -
+	* @output   -
+	* @author   Chakrit
+	* @Create Date 2564-08-13
+	* @Update Date 2564-08-19
+	*/
+    public function show_report()
+    {
+        $this->load->model('M_pef_group_nominee', 'grn');
+        $this->load->model('M_pef_group', 'grp');
+        $data['nominee'] = $this->grn->get_all_nominee()->result();
+        $data['obj_year'] = $this->grp->get_year()->result();
+        $this->output('consent/v_report_all', $data);
+    }
+
+    /*
+	* get_report
+	* get data report in year
+	* @input    year
+	* @output   data of report
+	* @author   Chakrit
+	* @Create Date 2564-08-13
+	* @Update Date 2564-08-18
+	*/
+    public function get_report()
+    {
+        $year = $this->input->post('year');
+        $this->load->model('M_pef_group', 'pef');
+        $data = $this->pef->get_data_year($year)->result();
+        echo json_encode($data);
+    }
+
+    /*
+	* get_section
+	* get data report in section
+	* @input    -
+	* @output   data of report
+	* @author   Chakrit
+	* @Create Date 2564-08-13
+	* @Update Date 2564-08-18
+	*/
+    public function get_section()
+    {
+        $this->load->model('M_pef_section', 'pef');
+        $data = $this->pef->get_all_section()->result();
+        echo json_encode($data);
+    }
+
+    /*
+	* show_report_detail
+	* show view report detail
+	* @input    sec_id
+	* @output   data of report detail
+	* @author   Chakrit
+	* @Create Date 2564-08-15
+	* @Update Date 2564-08-20
+	*/
+    public function show_report_detail($sec_id)
+    {
+        $this->load->model('M_pef_group', 'grp');
+        $this->load->model('M_pef_section', 'sec');
+        $this->load->model('M_pef_point_form', 'ptf');
+
+        $this->grp->sec_id = $sec_id;
+        $this->sec->sec_id = $sec_id;
+        $this->ptf->sec_id = $sec_id;
+
+        $data['sec_data'] = $this->grp->get_data_by_id()->result();
+        $data['ass_data'] = $this->sec->get_ass_by_sec_id()->result();
+        $data['point_data'] = $this->ptf->get_data_point()->result();
+        $this->output('consent/v_report_group', $data);
+    }
+
+    /*
+	* show_report_detail_assessor
+	* show view report detail assessor
+	* @input    grn_id
+	* @output   data of report detail assessor
+	* @author   Chakrit
+	* @Create Date 2564-08-15
+	* @Update Date 2564-08-20
+	*/
+    public function show_report_detail_assessor($grn_id)
+    {
+        $this->load->model('M_pef_group', 'grp');
+        $this->load->model('M_pef_section', 'sec');
+        $this->load->model('M_pef_point_form', 'ptf');
+
+        $this->grp->grn_id = $grn_id;
+        $this->sec->grn_id = $grn_id;
+        $this->ptf->grn_id = $grn_id;
+
+        $data['emp_data'] = $this->grp->get_emp_by_id()->row();
+        $data['ass_data'] = $this->sec->get_ass_by_nor_id()->result();
+        $data['point_data'] = $this->ptf->get_data_point_by_nor_id()->result();
+        $this->output('consent/v_report_detail_assessor', $data);
+    }
+
+
+    
     public function show_report_all()
     {
         $this->output('consent/v_report_all');
--- application/models/M_pef_group.php
+++ application/models/M_pef_group.php
@@ -1,5 +1,5 @@
 <?php
-    /* 
+/* 
     * M_pef_assessor
     * Model for 
     * @author Phatchara Khongthandee and Ponprapai Atsawanurak
@@ -55,4 +55,103 @@ class M_pef_group extends Da_pef_group
         $query = $this->db->query($sql);
         return $query;
     } //end  
+
+    /* Report
+    * get_year
+    * get data group in year
+    * @input    -
+    * @output   data group in year
+    * @author   Chakrit
+    * @Create Date 2565-04-10
+    */
+    public function get_year()
+    {
+        $sql = "SELECT *
+                FROM pefs_database.pef_group
+                GROUP BY YEAR(grp_date) ";
+        $query = $this->db->query($sql);
+        return $query;
+    }
+
+    /* Report
+    * get_data_year
+    * get data section, group, nominee, position in year
+    * @input    -
+    * @output   get data section, group, nominee, position in year
+    * @author   Chakrit
+    * @Create Date 2565-04-10
+    */
+    public function get_data_year($year)
+    {
+        $sql = "SELECT *
+                FROM pefs_database.pef_group AS grp
+                INNER JOIN pefs_database.pef_group_nominee AS grn
+                ON grp.grp_id = grn.grn_grp_id
+                INNER JOIN pefs_database.pef_section AS sec
+                ON sec.sec_id = grp.grp_position_group
+                INNER JOIN dbmc.position AS pos
+                ON pos.Position_ID = grn.grn_promote_to
+                WHERE YEAR(grp_date) = $year 
+                ORDER BY sec.sec_id";
+        $query = $this->db->query($sql);
+        return $query;
+    }
+
+    /* Report
+    * get_data_by_id
+    * get data section, group, nominee, position, employee, scetion_code, company
+    * @input    -
+    * @output   get data section, group, nominee, position, employee, scetion_code, company
+    * @author   Chakrit
+    * @Create Date 2565-04-11
+    */
+    public function get_data_by_id()
+    {
+        $sql = "SELECT * 
+                FROM pefs_database.pef_group AS grp
+                INNER JOIN pefs_database.pef_group_nominee AS grn
+                ON grp.grp_id = grn.grn_grp_id
+                INNER JOIN pefs_database.pef_section AS sec
+                ON sec.sec_id = grp.grp_position_group
+                INNER JOIN dbmc.employee AS emp
+                ON emp.Emp_ID = grn.grn_emp_id
+                INNER JOIN dbmc.position AS pos
+                ON pos.Position_ID = emp.Position_ID
+                INNER JOIN dbmc.sectioncode AS scode
+                ON emp.Sectioncode_ID = scode.Sectioncode
+                INNER JOIN dbmc.company AS com
+                ON emp.Company_ID = com.Company_ID
+                WHERE sec.sec_id = ?";
+        $query = $this->db->query($sql, array($this->sec_id));
+        return $query;
+    }
+
+    /* Report
+    * get_emp_by_id
+    * get data group, nominee, section, employee, position, section_code, company
+    * @input    -
+    * @output   get data group, nominee, section, employee, position, section_code, company
+    * @author   Chakrit
+    * @Create Date 2565-04-11
+    */
+    public function get_emp_by_id()
+    {
+        $sql = "SELECT * 
+                FROM pefs_database.pef_group AS grp
+                INNER JOIN pefs_database.pef_group_nominee AS grn
+                ON grn.grn_grp_id = grp.grp_id
+                INNER JOIN pefs_database.pef_section AS sec
+                ON sec.sec_id = grp.grp_position_group
+                INNER JOIN dbmc.employee AS emp
+                ON emp.Emp_ID = grn.grn_emp_id
+                INNER JOIN dbmc.position AS pos
+                ON pos.Position_ID = emp.Position_ID
+                INNER JOIN dbmc.sectioncode AS scode
+                ON emp.Sectioncode_ID = scode.Sectioncode
+                INNER JOIN dbmc.company AS com
+                ON emp.Company_ID = com.Company_ID
+                WHERE grn.grn_id = ?";
+        $query = $this->db->query($sql, array($this->grn_id));
+        return $query;
+    }
 }
