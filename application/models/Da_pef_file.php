246f6c5c8f6054b2bcc79af660bb65ab57e50f69
BroAnk <73092937+BroAnk@users.noreply.github.com>
2022-04-14 15:07:07 +0700

    AddFile
    View
    -v_add_file_present
    Controller
    -File_present_management
    M DA
    -Da_pef_file

--- application/models/Da_pef_file.php
+++ application/models/Da_pef_file.php
@@ -1,21 +1,54 @@
 <?php
-    /* 
-    * Da_pef_file
-    * Model for 
-    * @author Phatchara Khongthandee and Ponprapai Atsawanurak
-    * @Create Date 2565-03-04
-    */
-?>
+/*
+	* Da_pef_file_present.php
+    * add file present nominee
+    * @Author : Ponprapai and Thitima
+    * @Create Date : 2564-08-13
+    * @Update Date : 2564-08-14 
+*/
+defined('BASEPATH') or exit('No direct script access allowed');
 
-<?php
-include_once("pefs_model.php");
+include_once('pefs_model.php');
 
 class Da_pef_file extends pefs_model
-{
+{ //class Da_pef_addfile
 
-    function construct()
+
+    public function __construct()
     {
         parent::__construct();
     }
 
-}
+    /*
+	* insert_file
+	* insert file location, Employee id in database
+	* @input    fil_name and Emp_ID
+	* @output   fil_id
+	* @author   Ponprapai and Thitima
+	* @Create Date 2564-08-13
+	*/
+    function insert_file()
+    {
+        $sql = "INSERT INTO pefs_database.pef_file(fil_location,fil_emp_id) 
+        VALUES (?,?)";
+
+        $this->db->query($sql, array($this->fil_location, $this->fil_emp_id));
+    }
+
+    /*
+	* update_file
+	* update file location
+	* @input    fil_name and Emp_ID
+	* @output   New fil_name and Emp_ID
+	* @author   Ponprapai and Thitima
+	* @Create Date 2564-08-14 
+	*/
+    function update_file()
+    {
+        $sql = "UPDATE pefs_database.pef_file
+        SET fil_location= ?
+        WHERE fil_emp_id = ?";
+
+        $this->db->query($sql, array($this->fil_location, $this->fil_emp_id));
+    }
+}//end class Da_pef_addfile
