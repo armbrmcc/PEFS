dad15a829899959d9991381d0c7639470641d5ac
Thitima Popila <72957631+Thitima03@users.noreply.github.com>
2022-04-20 09:14:39 +0700

    Revert "update bug"
    This reverts commit 66abdfb182d4c668bfd380d8d9655fb7bbf2f848.

--- application/controllers/Result/Result.php
+++ application/controllers/Result/Result.php
@@ -31,12 +31,13 @@ class Result extends MainController
     public function show_result_group()
     {
         $ass_id = $_SESSION['UsEmp_ID'];
-
         $this->load->model('M_pef_group', 'pef');
         $this->load->model('M_pef_group_schedule', 'date');
-
         $data['arr_group'] = $this->pef->get_group_evaluation_by_id($ass_id)->result();
         $data['obj_date'] = $this->date->get_date_evaluation($ass_id)->result();
+        // echo "<pre>";
+        //     print_r($data['arr_group']);
+        // echo "</pre>";
 
         $this->output('consent/v_result_group_assessor', $data);
     }
@@ -55,7 +56,9 @@ class Result extends MainController
     {
         $this->load->model('M_pef_group_nominee', 'nominee');
         $data['arr_nominee'] = $this->nominee->get_nominee_detail($group_id)->result();
-
+        // echo "<pre>";
+        //     print_r($data['arr_nominee']);
+        // echo "</pre>";
         $this->output('consent/v_result_list_assessor', $data);
     } //show_result_list
 
@@ -67,40 +70,27 @@ class Result extends MainController
 	* @author  Thitima Popila and Ponprapai Atsawanurak
 	* @Create  Date 2565-01-25
     */
-    public function show_result_evaluation_type1($group_id, $assessor_id, $nominee_id, $promote, $emp)
+    public function show_result_evaluation_type1($group_id, $id_assessor, $id_nominee, $promote, $nor_id)
     {
-        $this->load->model('M_pef_group', 'pef');
         $this->load->model('M_pef_assessor', 'assessor');
         $this->load->model('M_pef_group_nominee', 'nominee');
-
         $this->load->model('M_pef_format_form', 'form');
-        $this->load->model('M_pef_description_form', 'des');
+        $this->load->model('M_pef_description_form', 'description');
         $this->load->model('M_pef_item_form', 'item');
-        $this->load->model('M_pef_performance_form', 'per');
-        $this->load->model('M_pef_point_form', 'point');
+
         
-        $data['obj_date'] = $this->pef->get_group_date_round($group_id)->result();
-        $data['obj_assessor'] = $this->assessor->get_assessor_by_id($assessor_id)->result();
-        $data['obj_group_ass'] = $this->assessor->get_assessor_detail($group_id)->result();
-        $data['obj_nominee'] = $this->nominee->get_nominee_by_id($nominee_id)->result();
-        $data['arr_nominee'] = $this->nominee->get_nominee_detail($group_id)->result();
+        $this->pos_id =$promote;
+        $this->pos_year = date("Y");
+        
         $data['arr_item'] = $this->item->get_item_evaluation_by_id()->result();
-
-        $data['obj_promote'] = $this->nominee->get_promote_to($nominee_id)->result();
+        $data['arr_des'] = $this->description->get_description_evaluation_by_id()->result();
+        $data['arr_nominee'] = $this->nominee->get_nominee_detail($group_id)->result();
+        $data['obj_assessor'] = $this->assessor->get_assessor_by_id($id_assessor)->result();
+        $data['obj_group_ass'] = $this->assessor->get_assessor_detail($group_id)->result();
+        $data['obj_nominee'] = $this->nominee->get_nominee_by_id($id_nominee)->result();
+        $data['obj_promote'] = $this->nominee->get_promote_to($id_nominee)->result();
         $data['arr_form'] = $this->form->get_evaluation_form($promote)->result();
-        $data['arr_des'] = $this->des->get_description_evaluation_by_id()->result();
-
-        $data['arr_date'] = $this->nominee->get_nominee_date($group_id)->result();
-        $date = $data['arr_date'][0]->grp_date;
-        // print_r($date);
-        // print_r($id_assessor);
-        // print_r($nor_id);
-        $data['arr_per'] = $this->per->get_performance($emp, $assessor_id, $date)->result();
-        // print_r($data['arr_per']);
-        $per_get =  $data['arr_per'][0]->per_id;
-        // print_r($per_get);
-        $data['arr_point'] = $this->point->get_point_list_round1($per_get)->result();
-
+    
         $this->output('consent/v_result_evaluation_assessor_round1', $data);
     } //show_result_evaluation_type1
 
@@ -119,9 +109,8 @@ class Result extends MainController
         $this->load->model('M_pef_format_form', 'form');
         $this->load->model('M_pef_description_form', 'description');
         $this->load->model('M_pef_item_form', 'item');
-        $this->load->model('M_pef_performance_form', 'performance');
-        $this->load->model('M_pef_point_form', 'point');
 
+        
         $this->pos_id =$promote;
         $this->pos_year = date("Y");
         
@@ -133,20 +122,6 @@ class Result extends MainController
         $data['obj_nominee'] = $this->nominee->get_nominee_by_id($id_nominee)->result();
         $data['obj_promote'] = $this->nominee->get_promote_to($id_nominee)->result();
         $data['arr_form'] = $this->form->get_evaluation_form($promote)->result();
-        
-        $date = $data['arr_date'][0]->grp_date;
-        // print_r($date);
-        // print_r($id_assessor);
-        // print_r($nor_id);
-
-        $data['arr_per'] = $this->performance->get_performance($nor_id, $id_assessor, $date)->result();
-        // print_r($data['arr_per']);
-        $data['arr_per_id'] = $this->performance->get_performance_by_id($nor_id, $id_assessor, $date)->result();
-        $per_get =  $data['arr_per'][0]->per_id;
-        // print_r($per_get);
-        $data['arr_point_round1'] = $this->point->get_point_list_round1($per_get)->result();
-        $data['arr_per'] = $this->performance->get_performance($nor_id, $id_assessor, $date)->result();
-        // print_r($data['arr_point_round1']);
 
         $this->output('consent/v_result_evaluation_assessor_round2_1', $data);
     } //show_result_evaluation_type2_1
