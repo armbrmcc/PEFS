229bcaff7d87a04e88e7464d6856a5b607fdbdc3
Pantakon <73054301+Pantakon@users.noreply.github.com>
2022-04-20 11:56:30 +0700

    Update Result.php

--- application/controllers/Result/Result.php
+++ application/controllers/Result/Result.php
@@ -223,7 +223,7 @@ class Result extends MainController
         $date_2 =  $data['date_2'][0]->grd_date;
         $data['arr_per2'] = $this->per->get_performance_result($Emp_ID, $ase_id, $date_2)->result();
         $per_get2 =  $data['arr_per2'][0]->per_id;
-        $data['arr_point_2'] = $this->point->get_point_list_round_2_admin($per_get,$Emp_ID, $ase_id, $date_2)->result();
+        $data['arr_point_2'] = $this->point->get_point_list_round_2_admin($per_get2,$Emp_ID, $ase_id, $date_2)->result();
         $this->output('consent/v_result_evaluation_assessor_round2_2', $data);
     } //show_result_evaluation_type2_2
 
